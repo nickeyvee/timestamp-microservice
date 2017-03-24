1dc6c65aeee3ec2d408a40a46fcd687c498dbd35
Nicholas  Vorraso <nvorraso2020@gmail.com>
2017-03-24 01:36:00 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+
+app.get( "/:date" , ( request, response ) => {
+
+  let naturalDate = new Date( request.params.date );
+  let date = new Date( request.params.date * 1000 );
+
+  const month = date.toString().split(' ')[1];
+  const day = date.toString().split(' ')[2];
+  const year = date.toString().split(' ')[3];
+
+
+ if ( date.toString() === "Invalid Date" && naturalDate.toString() === "Invalid Date" ) {
+   response.send( JSON.stringify(
+     { "unix" : null, "natural" : null }
+   ));
+ } else
+ if ( date.toString() === "Invalid Date" ) {
+   response.send( JSON.stringify(
+      { "unix" : naturalDate.getTime(),
+      "natural" : request.params.date }
+    ));
+  } else
+  if( naturalDate.toString() === "Invalid Date" ) {
+      response.send( JSON.stringify(
+        { "unix" : date.getTime(), "natural" : month + " " + day + ", " + year }
+      ));
+    }
+});
+
+app.get( "/", ( request, response ) => {
+    response.write( "TimeStamp Microservice!");
+    response.end();
+});
+
+app.listen( 3000 , ( data ) => {
+  console.log( 'listening on 3000' );
+});
